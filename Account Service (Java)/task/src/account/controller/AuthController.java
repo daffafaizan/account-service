d897df902268c3441d9c5091a57ef0d357f71d04
Daffa <daffamfzn@gmail.com>
2024-04-12 16:14:12 +0700

    refactor: removed unused annotation

--- Account Service (Java)/task/src/account/controller/AuthController.java
+++ Account Service (Java)/task/src/account/controller/AuthController.java
@@ -3,18 +3,14 @@ package account.controller;
 import account.dto.auth.Request.SignupRequest;
 import account.dto.auth.Response.SignupResponse;
 import account.service.auth.AuthService;
-import account.utils.Response;
-import account.utils.ResponseHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Component
 @RestController
 @RequestMapping("api/auth")
 public class AuthController {

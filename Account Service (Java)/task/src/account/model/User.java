61eba6599004ede00ae468011bc8925c2a1dbed2
Daffa <daffamfzn@gmail.com>
2024-04-22 00:23:26 +0700

    INTERNDEMO-85 added getters and setters to User model

--- Account Service (Java)/task/src/account/model/User.java
+++ Account Service (Java)/task/src/account/model/User.java
@@ -10,5 +10,25 @@ public class User {
     private long userId;
     private String name;
     private String lastname;
+    private String email;
     private String password;
+
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastname() {
+        return this.lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }

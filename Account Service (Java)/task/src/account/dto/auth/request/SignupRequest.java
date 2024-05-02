b10772abfa5cba16808f16a783534e59e4de485e
Daffa <daffamfzn@gmail.com>
2024-05-02 16:36:02 +0700

    INTERNDEMO-85 Removed messages at BeanValidation annotations

--- Account Service (Java)/task/src/account/dto/auth/request/SignupRequest.java
+++ Account Service (Java)/task/src/account/dto/auth/request/SignupRequest.java
@@ -5,18 +5,18 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public class SignupRequest {
-    @NotNull(message = "Name field is required!")
-    @NotBlank(message = "Name field can not be blank!")
+    @NotNull()
+    @NotBlank()
     private String name;
-    @NotNull(message = "Last name field is required!")
-    @NotBlank(message = "Last name field can not be blank!")
+    @NotNull()
+    @NotBlank()
     private String lastname;
-    @NotNull(message = "Email field is required!")
-    @NotBlank(message = "Email field can not be blank!")
-    @Email(message = "Email should be valid!", regexp = "^.+@acme.com$")
+    @NotNull()
+    @NotBlank()
+    @Email(regexp = "^.+@acme.com$")
     private String email;
-    @NotNull(message = "Password field is required!")
-    @NotBlank(message = "Password field can not be blank!")
+    @NotNull()
+    @NotBlank()
     private String password;
 
     public String getName() {

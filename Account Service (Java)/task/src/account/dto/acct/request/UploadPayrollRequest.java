bae9185b40f24d03eeaa4b9158a10581d0acaf29
Daffa <daffamfzn@gmail.com>
2024-05-03 16:08:41 +0700

    INTERNDEMO-85 Updated UploadPayrollRequest datatype

--- Account Service (Java)/task/src/account/dto/acct/request/UploadPayrollRequest.java
+++ Account Service (Java)/task/src/account/dto/acct/request/UploadPayrollRequest.java
@@ -3,24 +3,27 @@ package account.dto.acct.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import java.math.BigDecimal;
+import java.time.YearMonth;
+
 public class UploadPayrollRequest {
     @NotNull
     @NotBlank
     private String employee;
     @NotNull
     @NotBlank
-    private String period;
+    private YearMonth period;
     @NotNull
     @NotBlank
-    private Integer salary;
+    private BigDecimal salary;
 
     public String getEmployee() {
         return this.employee;
     }
-    public String getPeriod() {
+    public YearMonth getPeriod() {
         return this.period;
     }
-    public Integer getSalary() {
+    public BigDecimal getSalary() {
         return this.salary;
     }
 }

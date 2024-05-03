8a1502c27cfd4732599351994fbf1bf7b5134218
Daffa <daffamfzn@gmail.com>
2024-05-03 16:14:28 +0700

    INTERNDEMO-85 Added PayrollRepository

---
+++ Account Service (Java)/task/src/account/repository/PayrollRepository.java
@@ -0,0 +1,12 @@
+package account.repository;
+
+import account.model.Payroll;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PayrollRepository extends JpaRepository<Payroll, String> {
+    Optional<Payroll> findByEmailIgnoreCase(String email);
+}

0ef1b0d47a239643c2d40eacdcbb77b0ea9f8ec5
Daffa <daffamfzn@gmail.com>
2024-05-03 16:07:57 +0700

    INTERNDEMO-85 Created Payroll object model

---
+++ Account Service (Java)/task/src/account/model/Payroll.java
@@ -0,0 +1,49 @@
+package account.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.YearMonth;
+
+@Entity
+@Table(name = "payroll")
+public class Payroll {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer payrollId;
+    private String name;
+    private String lastname;
+    private YearMonth period;
+    private BigDecimal salary;
+
+    public Integer getPayrollId() {
+        return this.payrollId;
+    }
+    public void setPayrollId(Integer payrollId) {
+        this.payrollId = payrollId;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String lastname() {
+        return this.lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    public YearMonth getPeriod() {
+        return this.period;
+    }
+    public void setPeriod(YearMonth period) {
+        this.period = period;
+    }
+    public BigDecimal getSalary() {
+        return this.salary;
+    }
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+}

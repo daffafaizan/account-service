d0208da525fde8c95c20247682f3e4cd2dfe4bf9
Daffa <daffamfzn@gmail.com>
2024-05-03 11:05:14 +0700

    INTERNDEMO-85 Refactored password exception

--- Account Service (Java)/task/src/account/exception/auth/MinimumCharactersException.java
+++ Account Service (Java)/task/src/account/exception/auth/MinimumCharactersException.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class MinimumCharactersException extends RuntimeException {
     public MinimumCharactersException() {
-        super("Password length must be 12 chars minimum");
+        super("Password length must be 12 chars minimum!");
     }
 }

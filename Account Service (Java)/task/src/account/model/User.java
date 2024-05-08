5da5fb1a083c425043c2e3e5e07aefa8c0fa81e1
Daffa <daffamfzn@gmail.com>
2024-05-08 12:32:02 +0700

    INTERNDEMO-85 Updated User model fields with not null and not blank annotations

--- Account Service (Java)/task/src/account/model/User.java
+++ Account Service (Java)/task/src/account/model/User.java
@@ -1,26 +1,41 @@
 package account.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer userId;
+    private Integer id;
+    @NotNull
+    @NotBlank
     private String name;
+    @NotNull
+    @NotBlank
     private String lastname;
     @Email(regexp = "^.+@acme.com$")
+    @NotNull
+    @NotBlank
     private String email;
+    @JsonIgnore
+    @NotNull
+    @NotBlank
     private String password;
+    @JsonIgnore
+    @NotNull
+    @NotBlank
     private String authority;
 
     public Integer getId() {
-        return this.userId;
+        return this.id;
     }
-    public void setId(Integer userId) {
-        this.userId = userId;
+    public void setId(Integer id) {
+        this.id = id;
     }
     public String getName() {
         return this.name;

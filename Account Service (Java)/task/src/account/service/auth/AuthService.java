1c2061791650dbd5d7b97fb9e4c9aa76e84e6388
Daffa <daffamfzn@gmail.com>
2024-05-10 16:18:52 +0700

    INTERNDEMO-85 Refactor AuthService and remove residue from merge conflict

--- Account Service (Java)/task/src/account/service/auth/AuthService.java
+++ Account Service (Java)/task/src/account/service/auth/AuthService.java
@@ -1,12 +1,8 @@
 package account.service.auth;
 
-<<<<<<< HEAD
 import account.dto.auth.request.ChangePassRequestDTO;
-import account.dto.auth.request.SignupRequest;
-=======
 import account.dto.auth.request.SignupRequestDTO;
 import account.model.User;
->>>>>>> account-service-master
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.security.core.userdetails.UserDetails;
 

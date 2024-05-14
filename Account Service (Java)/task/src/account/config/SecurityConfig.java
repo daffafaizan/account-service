a0a0d30c8c7c95f857c69d9d2944c8eba210a83e
Daffa <daffamfzn@gmail.com>
2024-05-14 14:17:02 +0700

    INTERNDEMO-85 Updated authorization for existing endpoints

--- Account Service (Java)/task/src/account/config/SecurityConfig.java
+++ Account Service (Java)/task/src/account/config/SecurityConfig.java
@@ -27,9 +27,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth  // manage access
                         .requestMatchers(HttpMethod.POST, "/api/auth/signup").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/auth/changepass").authenticated()
-                        .requestMatchers(HttpMethod.GET, "/api/empl/payment", "/api/empl/payment/**").authenticated()
-                        .requestMatchers(HttpMethod.POST, "/api/acct/payments").permitAll()
-                        .requestMatchers(HttpMethod.PUT, "/api/acct/payments").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/empl/payment", "/api/empl/payment/**").hasAnyAuthority("Accountant", "User")
+                        .requestMatchers(HttpMethod.POST, "/api/acct/payments").hasAuthority("Accountant")
+                        .requestMatchers(HttpMethod.PUT, "/api/acct/payments").hasAuthority("Accountant")
                         .requestMatchers(HttpMethod.POST, "/actuator/shutdown").permitAll()
                         .anyRequest().denyAll()
 
